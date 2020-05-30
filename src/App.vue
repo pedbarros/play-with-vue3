7d70234c0283fe627583546065d99abc09756882
Pedro Barros <pedro.barros@herospark.com>
2020-05-30 12:09:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="toggleModalState">Open modal</button>
+  <teleport to="#modal-wrapper">
+    <modal v-if="modalOpen" @close="toggleModalState">
+      <p>Hello, I'm a modal window.</p>
+    </modal>
+  </teleport>
+</template>
+
+<script>
+import Modal from "./Modal.vue";
+import { ref } from 'vue'
+
+export default {
+  emits: [ "close" ],
+  setup () {
+    const modalOpen = ref(false)
+
+    const toggleModalState = () => {
+      modalOpen.value = !modalOpen.value
+    }
+
+    return {
+      modalOpen,
+      toggleModalState
+    }
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="modal">
+    <slot></slot>
+    <button @click="$emit('close')">Dismiss</button>
+  </div>
+</template>
+
+<style scoped>
+::v-slotted(p) {
+  font-style: italic;
+}
+</style>
